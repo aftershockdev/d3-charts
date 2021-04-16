631b0e4c5c911179dd06d1151585ca73f777d809
Ruslan Zherebytskyi <ruslanzh@sweetsoft.com.ua>
2021-04-16 18:51:36 +0300

    add bar multi-columns + default colors

--- src/modules/charts/bar.ts
+++ src/modules/charts/bar.ts
@@ -1,32 +1,66 @@
+import * as d3 from "d3";
 import { ChartVisualizer } from "../interfaces/charts";
 import { createScaleX, createScaleY } from "../chartsConfig/chart-scales";
+import options  from "../chartsConfig/chart-options";
 
 const drawBars: ChartVisualizer = (node, config, model, data, size): void => {
 
+    const palette = options.defaultColors;
 
-    const x = createScaleX(data, size,config, model);
-    const y = createScaleY(data, size,config, model);
+    const x = createScaleX(data, size, config, model);
+    const y = createScaleY(data, size, config, model);
 
-    const bars = node
+    const yCol = config.y;
+    const xCol = config.x;
+
+    const xLength = config.x.length;
+    const yLength = config.y.length;
+
+    if (yLength > 1) {
+        for (const i in yCol) {
+            const name = yCol[i];
+            node
+                .append("g")
+                .attr("transform", "translate(0,-5)")
+                .selectAll("rect")
+                .data(data)
+                .join("rect")
+                .attr("fill", palette[i])
+                .attr("class", name)
+                .attr("x", (d: any) => x(d.x))
+                .attr("y", (d: any) => y(d.y[name]))
+                .attr("height", (d: any) => y(0) - y(d.y[name]))
+                .attr("width",(size.width - size.margin.right) / data.length / 2);
+        }
+        return;
+    }
+    if (xLength > 1) {
+        for (const i in xCol) {
+            const name = xCol[i];
+            node
+                .append("g")
+                .attr("transform", "translate(0,-5)")
+                .selectAll("rect")
+                .data(data)
+                .join("rect")
+                .attr("fill", palette[i])
+                .attr("class", name)
+                .attr("x", (d: any) => x(d.x[name]))
+                .attr("y", (d: any) => y(d.y))
+                .attr("height", (d: any) => y(0) - y(d.y))
+                .attr("width",(size.width - size.margin.right) / data.length);
+        }
+        return;
+    }
+
+    node
         .append("g")
         .attr("transform", "translate(0,-5)")
         .style("mix-blend-mode", "multiply")
-        .append("g")
         .selectAll("rect")
         .data(data)
         .join("rect")
-        .attr("fill", "#cccccc");
-
-
-    // if(horizontal){
-    //     bars
-    //         .attr("x", size.margin.left )
-    //         .attr("y", (d: any) => y(d.y))
-    //         .attr("width", (d: any) => x(d.x) - size.margin.left)
-    //         .attr("height", (size.height - size.margin.bottom) / data.length )
-    //     return;
-    // }
-    bars
+        .attr("fill", palette[0])
         .attr("x", (d: any) => x(d.x))
         .attr("y", (d: any) => y(d.y))
         .attr("height", (d: any) => y(0) - y(d.y))

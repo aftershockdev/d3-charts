9eea779d351c29c304f8338f60fc4cf9bf22148f
Ruslan Zherebytskyi <ruslanzh@sweetsoft.com.ua>
2021-04-21 18:59:03 +0300

    refactoring data module

--- src/modules/chartsConfig/chart-data.ts
+++ src/modules/chartsConfig/chart-data.ts
@@ -11,11 +11,9 @@ interface IValue {
 function getColProperties(colArr: string[], el: IElement, columns: { [key: string]: IColumnModel }): IElement {
     const result: IElement = {};
     colArr.forEach(name => {
-        const elV = el[name];
-        const value = columns[name].dataType === DataTypeEnum.date
-            ? new Date(elV)
-            : elV;
-        result[name] = value;
+        result[name] = columns[name].dataType === DataTypeEnum.date
+            ? new Date(el[name])
+            : el[name];
     });
     return result;
 }
@@ -26,22 +24,13 @@ export const chartDataConfiguration = (data: IElement[], config: IChartConfigura
     const xCol  = config.x;
     const yCol  = config.y;
 
-    const firstNameX = xCol[0];
-    const firstNameY = yCol[0];
-
     const xLength = xCol.length;
     const yLength = yCol.length;
 
     const allColumnsName = [...xCol, ...yCol];
-    const numberColumns: string[] = [];
 
     if (xLength > 1 && yLength > 1)
         throw new Error("One of the axes must be with one column");
-
-    allColumnsName.forEach(key => columns[key].dataType === DataTypeEnum.number ? numberColumns.push(key) : null);
-
-    if(!numberColumns.length)
-        throw new Error("One of the columns must be of type number");
 
     const dataWithoutEmptyProp = data.filter(el => {
         allColumnsName.forEach(name => {
@@ -60,33 +49,22 @@ export const chartDataConfiguration = (data: IElement[], config: IChartConfigura
         return el;
     });
 
+    const firstNameX = xCol[0];
+    const firstNameY = yCol[0];
+
     const columnTypeX = columns[firstNameX].dataType;
     const columnTypeY = columns[firstNameY].dataType;
 
     return dataWithoutEmptyProp.map(el => {
-        let x = el[firstNameX];
-        let y = el[firstNameY];
+        let x = columnTypeX === DataTypeEnum.date ? new Date(el[firstNameX]): el[firstNameX];
+        let y = columnTypeY === DataTypeEnum.date ? new Date(el[firstNameY]): el[firstNameY];
 
         if (yLength > 1) {
-            x = columnTypeX === DataTypeEnum.date
-                ? new Date (el[firstNameX])
-                : el[firstNameX];
             y = getColProperties(yCol, el, columns);
         }
 
         if (xLength > 1) {
-            y = columnTypeY === DataTypeEnum.date
-                ? new Date (el[firstNameY])
-                : el[firstNameY];
             x = getColProperties(xCol, el, columns);
-        }
-
-        if (!(x && y))
-            return null;
-
-        if (yLength === 1 && xLength === 1) {
-            columnTypeX === DataTypeEnum.date ? x = new Date(x) : null;
-            columnTypeY === DataTypeEnum.date ? y = new Date(y) : null;
         }
 
         return  { x, y };

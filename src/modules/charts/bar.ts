06b361626aea054dd5084c0219324276577d68b3
iash44 <iash44@users.noreply.github.com>
2021-04-21 10:08:46 +0300

    minor use simple return for const functions

--- src/modules/charts/bar.ts
+++ src/modules/charts/bar.ts
@@ -2,7 +2,7 @@ import { ChartVisualizer, DataTypeEnum } from "../interfaces/charts";
 import options  from "../chartsConfig/chart-options";
 
 const drawBars: ChartVisualizer = (node, config, model, data, size, xScale, yScale): void => {
-    const {columns} = model;
+    const { columns } = model;
     const palette = options.defaultColors;
 
     const yCol = config.y;
@@ -13,44 +13,39 @@ const drawBars: ChartVisualizer = (node, config, model, data, size, xScale, ySca
 
     const distance = data.length;
 
-    const bar = (i: number) => {
-        return node
+    const bar = (i: number) =>
+        node
             .append("g")
             .attr("transform", "translate(0,-5)")
             .selectAll("rect")
             .data(data)
             .join("rect")
             .attr("fill", palette[i]);
-    };
 
-    const barsY = (key: string, i: number) => {
-        return bar(i)
+    const barsY = (key: string, i: number) =>
+        bar(i)
             .attr("x", (d: any) => xScale(d.x))
             .attr("y", (d: any) => yScale(d.y[key] ? d.y[key] : d.y))
             .attr("height", (d: any) => yScale(0) - yScale(d.y[key] ? d.y[key] : d.y))
             .attr("width",(size.width - size.margin.right) / distance / 1.5);
-    };
 
-    const barsX = (key: string, i: number) => {
-        return bar(i)
+    const barsX = (key: string, i: number) =>
+        bar(i)
             .attr("x", size.margin.left )
             .attr("y", (d: any) => yScale(d.y))
             .attr("width", (d: any) => xScale(d.x[key] ? d.x[key] : d.x ) - size.margin.left)
             .attr("height", (size.height - size.margin.bottom) / distance / 1.5);
-    };
 
     if (yLength > 1) {
-        yCol.forEach((key, i) => {
-            barsY(key, i);
-        });
+        yCol.forEach(barsY);
         return;
     }
+
     if (xLength > 1) {
-        xCol.forEach((key, i) => {
-            barsX(key, i);
-        });
+        xCol.forEach(barsX);
         return;
     }
+
     if (xLength == 1 && yLength == 1) {
         const xType = columns[xCol[0]].dataType;
         const yType = columns[yCol[0]].dataType;
@@ -59,6 +54,7 @@ const drawBars: ChartVisualizer = (node, config, model, data, size, xScale, ySca
             barsX("", 1);
             return;
         }
+
         if (yType === DataTypeEnum.number && xType !== DataTypeEnum.number) {
             barsY("", 1);
             return;
